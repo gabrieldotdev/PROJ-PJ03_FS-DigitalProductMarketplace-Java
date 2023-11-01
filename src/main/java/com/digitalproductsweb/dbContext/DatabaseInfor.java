d298eaed6ed3180cda29a62355e04b4de2be276e
lilwxs <124787909+lilwxs@users.noreply.github.com>
2023-11-02 03:45:08 +0700

    UPLOAD

--- src/main/java/com/digitalproductsweb/dbContext/DatabaseInfor.java
+++ src/main/java/com/digitalproductsweb/dbContext/DatabaseInfor.java
@@ -2,7 +2,7 @@ package com.digitalproductsweb.dbContext;
 
 public interface DatabaseInfor {
     public static String driverName = "com.mysql.cj.jdbc.Driver";
-    public static String url = "jdbc:mysql://fishshop.mysql.database.azure.com:3306/digitalproducts?useSSL=true";
+    public static String url = "jdbc:mysql://<add server>/digitalproducts?useSSL=true";
     public static String user = "maity";
     public static String pass = "ConCaNon123";
 }
